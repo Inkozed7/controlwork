de389de2510a70f419057e4210fd24bfb2929ad3
Inkozed7 <107644797+Inkozed7@users.noreply.github.com>
2022-08-08 18:26:08 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿void PrintArray(string[] array)
+{   
+    int n = array.Length;
+    Console.Write("[");
+    for(int i=0;i<n;i++)
+    {
+        if(array[i]==null) continue;
+        if(i==0) Console.Write(array[i]);
+        else Console.Write(", "+array[i]);
+    }
+    Console.Write("]");
+};
+
+string[] FilterArray(string[] array)
+{   
+    int size = array.Length;
+    string[] result = new string[size];
+    int index = 0;
+    int index_result = 0;
+
+    while (index<size)
+    {  
+        if(array[index].Length<=3)
+        {
+            result[index_result]=array[index];
+            index_result++;
+        }
+        index++;
+    } 
+    return result;
+};
+
+string[] array = new string[]
+{"Russia","Inkozed","Ekb"};
+PrintArray(array);
+Console.Write("->");
+PrintArray(FilterArray(array));
